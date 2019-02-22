5d37b03619140cbff6d6a84bca238edc2d097a58
Nile Walker <nile@Niles-MacBook-Pro.local>
2019-02-22 11:35:55 -0500

    Initial Commit

---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+# MSU Announce Parser
+#### _Showing Students Relevant Events_
+### Data Structures Used
+* Opp
+* Functions
+### Psuedo Code
+* Implement Logic in python
+# To include
+
+# Tasks 
+* Determine how to find relevant events in the email
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+/*imaps conn("imap.gmail.com", 993);
+conn.authenticate("mailio@gmail.com", "mailiopass", imap::auth_method_t::LOGIN);
+message msg;
+conn.fetch("inbox", 1, msg);
+*/
+int main(){
+    //Get Login Info
+    std::cout<<0<<"\n";
+}
